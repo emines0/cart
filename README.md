ac2743bb2458fee4efbcfd75c617fc33fffcd080
emines0 <emin.sefa1@gmail.com>
2023-02-17 16:02:44 +0100

    increase/decrease item count functionality

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cart project
+
+- setup reducer
+- use reducer functionality for
+  - clear cart,
+  - remove item,
+  - increase, decrease amount of item in the cart
+  - change total value of price
--- src/reducer.js
+++ src/reducer.js
@@ -3,13 +3,59 @@ const reducer = (state, action) => {
     return {
       ...state,
       cart: [],
+      /*
+       * If action is clear Cart return an empty array of cart
+       */
     }
   }
   if (action.type === 'REMOVE') {
     return {
       ...state,
       cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+      /*
+       * If the id matches with payload id return filtered cartItems
+       */
     }
+  }
+
+  if (action.type === 'INCREASE') {
+    let tempCart = state.cart.map((cartItem) => {
+      /*
+       * iterating over the state before change
+       */
+
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 }
+        /*
+         * If the id matches with payload id return new array
+         * with cartItems an increase amount by 1
+         */
+      }
+      return cartItem
+    })
+
+    return { ...state, cart: tempCart }
+  }
+
+  if (action.type === 'DECREASE') {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        /*
+         * iterating over the state before change
+         */
+
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 }
+          /*
+           * If the id matches with payload id return new array
+           * with cartItems an decrease amount by 1
+           */
+        }
+        return cartItem
+      })
+      .filter((cartItem) => cartItem.amount !== 0)
+
+    return { ...state, cart: tempCart }
   }
   return state
 }
